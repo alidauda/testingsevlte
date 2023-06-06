e47d90f254f788df72afa2eebd06ccc032d20e68
alidauda <alidauda14@gmail.com>
2023-06-06 22:39:19 +0100

    first commit

---
+++ src/routes/signIn/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import "@unocss/reset/tailwind.css"; // https://github.com/unocss/unocss#style-resetting
+    import "uno.css"; // https://github.com/unocss/unocss/tree/main/packages/vite#installation
+  </script>
+  <div class="h-screen w-full text-black bg-gray">
+    <slot />
+  </div>
---
+++ src/routes/signIn/+page.svelte
@@ -0,0 +1,17 @@
+
+<h1 class="font-(poppins medium)">sign in</h1>
+<h2>welcome</h2>
+<h3>worked</h3>
+<div class="grid">
+    <form method="POST" >
+        <label>
+            Email
+            <input name="email" type="email">
+        </label>
+        <label>
+            Password
+            <input name="password" type="password">
+        </label>
+        <button>Log in</button>
+    </form>
+</div>
